ca6351f0a6e540685d49209f150dd2b5521a48d5
Eduard Marbach <mail@eduard-marbach.de>
2021-12-07 10:47:16 +0100

    fix: PR comment changes

--- src/utils.ts
+++ src/utils.ts
@@ -7,9 +7,6 @@ const httpsProxy = process.env["HTTPS_PROXY"] || process.env["https_proxy"];
 let proxyConf = {};
 
 if (httpProxy || httpsProxy) {
-  console.log(`Found proxy setting. Using to download files.`);
-  console.log(`HTTP: ${httpProxy}`);
-  console.log(`HTTPS: ${httpProxy}`);
   proxyConf = {
     proxy: false,
     httpAgent: httpProxy ? new ProxyAgent(httpProxy) : undefined,
@@ -19,16 +16,10 @@ if (httpProxy || httpsProxy) {
 
 const axios = axiosBase.create({ ...proxyConf });
 
-export async function request(
+export async function request<T = any>(
   url: string,
   options: AxiosRequestConfig = {}
-): Promise<any> {
-  console.log(`Request to: ${url} with options: ${JSON.stringify(options)}`);
-  try {
-    const result = await axios.get(url, { ...options });
-    return result.data;
-  } catch (e: any) {
-    console.error(e.toJSON());
-    throw new Error(`Request for terraform binary failed: ${e && e.message}`);
-  }
+): Promise<T> {
+  const result = await axios.get(url, { ...options });
+  return result.data;
 }
