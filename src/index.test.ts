a0f80383135e7e534fa91ac697eeb0c0f5ea6321
Daniel Banck <daniel@dbanck.de>
2022-03-14 11:40:31 +0100

    add test case for checksum download

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
 import * as assert from 'assert';
 import * as path from 'path';
 
-import { Release, getRelease } from './index';
+import { Release } from './index';
 
 describe('LS installer', () => {
 	it('should calculate correct file sha256 sum', async () => {
@@ -13,4 +13,34 @@ describe('LS installer', () => {
 		assert.strictEqual(sum, expectedSum);
 	});
 
+	it('should download the correct sha256 sum', async () => {
+		const validSum =
+			'8629ccc47ee8d4dfe6d23efb93b293948a088a936180d07d3f2ed118f6dd64a5';
+		const release = new Release({
+			name: 'terraform-ls',
+			version: '0.25.2',
+			shasums: 'terraform-ls_0.25.2_SHA256SUMS',
+			shasums_signature: 'terraform-ls_0.25.2_SHA256SUMS.sig',
+			shasums_signatures: [
+				'terraform-ls_0.25.2_SHA256SUMS.72D7468F.sig',
+				'terraform-ls_0.25.2_SHA256SUMS.sig',
+			],
+			builds: [
+				{
+					name: 'terraform-ls',
+					version: '0.25.2',
+					os: 'darwin',
+					arch: 'amd64',
+					filename: 'terraform-ls_0.25.2_darwin_amd64.zip',
+					url: 'https://releases.hashicorp.com/terraform-ls/0.25.2/terraform-ls_0.25.2_darwin_amd64.zip',
+				},
+			],
+		});
+
+		const remoteSum = await release.downloadSha256Sum(
+			release.builds[0].filename
+		);
+
+		assert.strictEqual(remoteSum, validSum);
+	});
 });
