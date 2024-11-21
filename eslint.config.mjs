405c1da57dfa1107e974ae33f3771a481e0a2458
James Pogran <jpogran@outlook.com>
2024-11-21 09:46:21 -0500

    eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import pluginJs from '@eslint/js';
 import globals from 'globals';
+import prettierConfig from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  prettierConfig,
   {
     ignores: ['jest.config.js', 'dist', 'out', '**/node_modules/**', '**/out/**', '**/dist/**'],
   },
